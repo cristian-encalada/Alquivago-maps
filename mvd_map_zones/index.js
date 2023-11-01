184fc8619a1d2b8ef29567fa2d22c8eea4478c3e
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-31 21:55:10 -0300

    mvd_map_rent and mvd_map_zones, last version

--- mvd_map_zones/index.js
+++ mvd_map_zones/index.js
@@ -2,8 +2,10 @@ async function initMap() {
   // Request needed libraries.
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.901112, lng: -56.164532 },
-    zoom: 13
+    // center: { lat: -34.901112, lng: -56.164532 }, // Montevideo
+    // center: { lat: -34.86574, lng: -56.17798 }, // Brazo Oriental - Zoom 13
+    center: { lat: -34.8167, lng: -56.1833 }, // Peñarol - Zoom 12
+    zoom: 12
   });
 
   // Fetch the coordinates from 'mvd_coordinates' CSV file
@@ -60,7 +62,7 @@ async function addPolygon(map, coordinates, name, apiData) {
     const zoneData = apiData.rents.find(rent => rent.zona === name);
 
     if (zoneData) {
-      infoWindow.setContent(`${name}<br>Number of rents: ${zoneData.cantidad}`);
+      infoWindow.setContent(`${name}<br>Alquileres: ${zoneData.cantidad}`);
     } else {
       infoWindow.setContent(name);
     }
